a02696a065e8720190af7b5fadab02da9250e4ab
codepilot386 <@gmail.com>
2026-08-05 20:41:21 +0900

    예제 추가

---
+++ C-language-study/02_prinf_scanf/ex01_printf_scanf_one.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int age;
+
+    printf("나이를 입력하세요:");
+    scanf("%d",&age);
+
+    printf("내 나이는 %d살 입니다.", age);
+    return 0;
+}
